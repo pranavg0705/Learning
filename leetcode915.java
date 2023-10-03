07770347f10351fe043056215189e35dca8d077d
Pranav <spnap2004@gmail.com>
2023-10-03 16:38:12 +0530

    leet code sol 915

---
+++ leetcode915.java
@@ -0,0 +1,19 @@
+class leetcode915 {
+    public int partitionDisjoint(int[] nums) {
+        int n = nums.length;
+        int i, max = nums[0], pos = 0;
+        for (i = 0; i < n; i++) {
+            if (nums[i] < max)
+                pos = i;
+        }
+        for (i = 0; i < pos; i++) {
+            if (nums[i] > max)
+                max = nums[i];
+        }
+        for (i = 0; i < n; i++) {
+            if (nums[i] < max)
+                pos = i;
+        }
+        return pos + 1;
+    }
+}
