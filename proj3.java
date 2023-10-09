129352fe690099e3a60b18b4a6d5dea58f0890c2
Pranav <spnap2004@gmail.com>
2023-10-09 18:39:04 +0530

    Deleted

--- proj3.java
+++
@@ -1,6 +0,0 @@
-public class proj3 {
-    public static void main(String[] args) {
-        int a = 5, b = 5;
-        System.out.println(a * b);
-    }
-}
--- proj4.java
+++
@@ -1,6 +0,0 @@
-public class proj4 {
-    public static void main(String[] args) {
-        int a = 2, b = 10;
-        System.out.println(b / a);
-    }
-}
